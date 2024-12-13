21fe619303a4ac807e7b79706d5f49757ee23c37
douglascastros <douglashenriquees@gmail.com>
2024-12-13 18:19:43 -0300

    implementing display property samples

--- js/display-property.js
+++ js/display-property.js
@@ -6,19 +6,19 @@ radios.forEach(radio => {
 
         Array.prototype.forEach.call(changes, function (change) {
             change.style.setProperty('display', e.target.value);
-
-            change.classList.remove('slide-down');
-            setTimeout(function () { change.classList.add('slide-down'); });
-
             document.getElementById('display-prop-changed').innerText = e.target.value;
 
-            let textExplaining = document.getElementById('text-explaining');
+            const textExplaining = document.getElementById('text-explaining');
             if (e.target.value === 'block') {
                 textExplaining.innerHTML = `the element fills the entire horizontal size of the screen, even if its width is smaller than the screen width. you cannot place elements side by side, but you can set width and height.`;
             }
             else {
                 textExplaining.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tempora excepturi vel molestias facere voluptate, explicabo fugit veniam, dolor reiciendis iusto nobis vero voluptatibus porro. Eligendi nesciunt et voluptate ipsum.`;
             }
+
+            const slideDown = document.getElementsByClassName('display-prop')[0];
+            slideDown.classList.remove('slide-down');
+            setTimeout(function () { slideDown.classList.add('slide-down'); });
         });
     });
 });

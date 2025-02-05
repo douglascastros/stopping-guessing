2f7d41fa545d5cd3e38060524e8c7c7f7864c5fe
douglascastros <douglashenriquees@gmail.com>
2025-02-05 16:34:31 -0300

    implementing display property samples

--- js/float-property.js
+++ js/float-property.js
@@ -1,4 +1,5 @@
 const optionsFloat = document.getElementsByName('float-property');
+const optionsFloatSecond = document.getElementsByName('float-property-second');
 
 optionsFloat.forEach(option => {
     option.addEventListener('click', function (e) {
@@ -8,17 +9,37 @@ optionsFloat.forEach(option => {
             item.style.setProperty('float', e.target.value);
             document.getElementById('float-prop-changed').innerText = e.target.value;
 
-            const sectionFloatProp = document.getElementsByClassName('float-prop')[0];
-            const floatPropItemSecond = document.getElementById('floatSecond');
             const textExplaining = document.getElementById('text-explaining-float');
 
             if (e.target.value === 'none') {
-                sectionFloatProp.style.setProperty('padding-bottom', '0');
                 textExplaining.innerHTML = ``;
             }
             else if (e.target.value === 'left') {
-                sectionFloatProp.style.setProperty('padding-bottom', '50px');
-                textExplaining.innerHTML = `the float effect puts elements in a context ahead, so elements from the original context take the place of elements that went to the higher context, but their text did not.`;
+                textExplaining.innerHTML = `the float effect puts elements in a context ahead, so elements from the original context take the place of elements that went to the higher context, but their content did not.`;
+            }
+
+            const slideDown = document.getElementsByClassName('float-prop')[0];
+            slideDown.classList.remove('slide-down');
+            setTimeout(function () { slideDown.classList.add('slide-down'); });
+        });
+    });
+});
+
+optionsFloatSecond.forEach(option => {
+    option.addEventListener('click', function (e) {
+        const items = document.getElementsByClassName('float-prop-item -second');
+
+        Array.prototype.forEach.call(items, function (item) {
+            item.style.setProperty('float', e.target.value);
+            document.getElementById('float-prop-changed-second').innerText = e.target.value;
+
+            const textExplaining = document.getElementById('text-explaining-float');
+
+            if (e.target.value === 'none') {
+                textExplaining.innerHTML = ``;
+            }
+            else if (e.target.value === 'left') {
+                textExplaining.innerHTML = `the float effect puts elements in a context ahead, so elements from the original context take the place of elements that went to the higher context, but their content did not.`;
             }
 
             const slideDown = document.getElementsByClassName('float-prop')[0];
@@ -29,3 +50,4 @@ optionsFloat.forEach(option => {
 });
 
 document.getElementById('float-none').click();
+document.getElementById('float-none-second').click();

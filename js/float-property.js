3cde5964501464e26318d1e5fa6469efc9a0f487
douglascastros <douglashenriquees@gmail.com>
2025-03-14 17:54:49 -0300

    implementing display property samples

--- js/float-property.js
+++ js/float-property.js
@@ -1,5 +1,4 @@
 const optionsFloat = document.getElementsByName('float-property');
-const optionsFloatSecond = document.getElementsByName('float-property-second');
 
 optionsFloat.forEach(option => {
     option.addEventListener('click', function (e) {
@@ -28,29 +27,4 @@ optionsFloat.forEach(option => {
     });
 });
 
-optionsFloatSecond.forEach(option => {
-    option.addEventListener('click', function (e) {
-        const items = document.getElementsByClassName('float-prop-item -second');
-
-        Array.prototype.forEach.call(items, function (item) {
-            item.style.setProperty('float', e.target.value);
-            document.getElementById('float-prop-changed-second').innerText = e.target.value;
-
-            const textExplaining = document.getElementById('text-explaining-float');
-
-            if (e.target.value === 'none') {
-                textExplaining.innerHTML = `nothing happening here...`;
-            }
-            else if (e.target.value === 'left') {
-                textExplaining.innerHTML = `the float effect puts elements in a context ahead, so elements from the original context take the place of elements that went to the higher context, but their content did not.`;
-            }
-
-            const slideDown = document.getElementsByClassName('float-prop')[0];
-            slideDown.classList.remove('slide-down');
-            setTimeout(function () { slideDown.classList.add('slide-down'); });
-        });
-    });
-});
-
 document.getElementById('float-none').click();
-document.getElementById('float-none-second').click();
